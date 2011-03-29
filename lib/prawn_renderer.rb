92aa4deab0f9292d4c4c519c6faa867e248af83a
Peter Haight <peterh@giantrabbit.com>
2011-03-29 00:31:29 -0700

    Decided I need to wrap the PDF renderer so I can provide access to helper methods. For some reason y isn't showing up in method_missing so I made a pos_y hack to return y for the time being.

--- lib/prawn_renderer.rb
+++ lib/prawn_renderer.rb
@@ -1,19 +1,47 @@
 require 'prawn'
+require 'prawn/measurement_extensions'
+require 'prawn/table'
+
 module PrawnRenderer
+  class DocumentWrapper
+    def initialize(view, template)
+      @view = view
+      @template = template
+      @pdf = Prawn::Document.new
+    end
+
+    def method_missing(symbol, *args, &block)
+      if @pdf.respond_to?(symbol)
+	@pdf.send(symbol, *args, &block)
+      elsif @view.respond_to?(symbol)
+	@view.send(symbol, *args, &block)
+      else
+	super
+      end
+    end
+
+    def pos_y
+      @pdf.y
+    end
+
+    def render
+      for instance_variable_name in @view.instance_variables
+	instance_variable_sym = instance_variable_name.to_sym
+	unless instance_variable_defined?(instance_variable_sym)
+	  instance_variable_set(instance_variable_sym, @view.instance_variable_get(instance_variable_sym))
+	end
+      end
+      eval(@template.source)
+      @pdf.render
+    end
+  end
+
   class TemplateHandler < ActionView::TemplateHandler
     include ActionView::TemplateHandlers::Compilable
     def compile(template)
       code = <<-EOS
-	pdf = Prawn::Document.new
-	for instance_variable_name in instance_variables
-	  unless pdf.instance_variable_defined?(instance_variable_name.to_sym)
-	    pdf.instance_variable_set(instance_variable_name.to_sym, instance_variable_get(instance_variable_name.to_sym))
-	  end
-	end
-	pdf.instance_eval do
-	  #{template.source}
-	end
-	pdf.render
+	doc = PrawnRenderer::DocumentWrapper.new(self, template)
+	doc.render
       EOS
     end
   end
